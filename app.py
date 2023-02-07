b6b05da38f06120674ac565dd40dbda7549dc758
christianmorris <cgmorris@terpmail.umd.edu>
2023-02-06 19:51:04 -0500

    Added new route for hello

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World! from Christian Morris I am adding my first code change'
 
+@app.route('/hello')
+def hello():  # put application's code here
+    return render_template('hello.html')
+
 
 if __name__ == '__main__':
     app.run()
