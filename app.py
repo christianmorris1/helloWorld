1d67d9eaa8193506382991987acec2d2a89f423b
christianmorris <cgmorris@terpmail.umd.edu>
2023-02-06 20:27:12 -0500

    Added change to help HTML success

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():  # put application's code here.
     return 'Hello World! from Christian Morris I am adding my first code change'
 
 @app.route('/hello')
