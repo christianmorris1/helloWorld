dbafb5e5a76fac7479f06e930bef6b2946bd4446
christianmorris <cgmorris@terpmail.umd.edu>
2023-02-20 20:43:51 -0500

    Made changes to app.py to reflect new HTML files in git repository

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ def hello():  # put application's code here
     return render_template('hello.html')
 
 @app.route('/about')
-def hello():  # put application's code here
-    return render_template('hello.html')
+def about():  # put application's code here
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
