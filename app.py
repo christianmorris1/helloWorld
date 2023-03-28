99c0cf741c9c521e0a69f3f298e141449de911c7
christianmorris <cgmorris@terpmail.umd.edu>
2023-03-28 17:26:27 -0400

    Made changes to HTML from class session, committing and pushing to save progress.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,22 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here.
     return 'Hello World! from Christian Morris I am adding my first code change'
+
+@app.route('/favorite-course')
+def favorite_course():
+    print('You entered your favorite course as:' + request.args.get('subject'))
+    print('With the course number:' + request.args.get('c_number'))
+
+    return render_template('favorite-course.html')
+
+@app.route('/contact', methods = ['GET','POST'] )
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
+
+
 
 @app.route('/hello')
 def hello():  # put application's code here
